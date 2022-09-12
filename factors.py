7edcdb51cc0faa8956800c7392b610db0b736d87
jude iwuji <judeiwuji@gmail.com>
2022-09-12 22:08:30 +0100

    Add: Task 0

---
+++ factors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from sys import argv, stderr, exit
+
+
+def factorize(n):
+	p, q = 1, n
+	i = 2
+	while i <= (n/2):
+		if n % i == 0:
+			p, q = i, int(n / i)
+			break
+		i += 1
+	return (p, q)
+
+def main():
+	if(len(argv) != 2):
+		stderr.write("Usage: factors <file>\n")
+		exit(1)
+	with open(argv[1]) as file:
+		for n in file:
+			n = int(n)
+			p, q = factorize(n)
+			print("{:d}={:d}*{:d}".format(n, q, p))
+
+if __name__ == "__main__":
+	main()
